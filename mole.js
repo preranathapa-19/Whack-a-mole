1bdcdb8968d31769073ee74ce2fc4d4a221e08be
prerana <preranamagar19@gmail.com>
2026-02-24 22:06:25 +0545

    js add

--- mole.js
+++ mole.js
@@ -1,4 +1,4 @@
-
+let currMoleTile;
 
 
 window.onload = function(){
@@ -7,8 +7,27 @@ window.onload = function(){
 
 function setGame(){
 //set up the grid for the game boaard in html
-    for(let i=1; i<=9; i++){ 
-
+    for(let i=1; i<=9; i++){ // i goes from 0 to 8, stop at 9
+        // div id= "0-8"
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        document.getElementById("board").appendChild(tile);
     }
-    
+    setInterval(setMole, 2000);  //2000 miliseconds = 2 seconds
+}
+
+function getRandomTile() {
+    //math.random --> (0-1) *9= (0-9) --> round down to (0-8)integers
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    let mole = document.createElement("img");
+    mole.src = "./monty-mole.png";
+
+    let num = getRandomTile();
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+
 }
