8c0a065e533ef6792869f441011d3466ac31b524
prerana <preranamagar19@gmail.com>
2026-02-27 21:58:36 +0545

    Game complete-ready to  play

--- mole.js
+++ mole.js
@@ -9,22 +9,24 @@ window.onload = function() {
 
 function setGame() {
     //set up the grid in html
-    for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
+    for (let i = 0; i < 9; i++) {   //i goes from 0 to 8, stops at 9
         //<div id="0-8"></div>
         let tile = document.createElement("div");
         tile.id = i.toString();
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
-    setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
+    setInterval(setMole, 1000);  // 1000 miliseconds = 1 second, every 1 second call setMole
+    setInterval(setPlant, 2000);   // 2000 miliseconds = 2 seconds, every 2 second call setPlant
 }
+
 
 function getRandomTile() {
     //math.random --> 0-1 --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
     let num = Math.floor(Math.random() * 9);
     return num.toString();
-}
+} 
+
 
 function setMole() {
     if (gameOver) {
@@ -44,6 +46,7 @@ function setMole() {
     currMoleTile.appendChild(mole);
 }
 
+
 function setPlant() {
     if (gameOver) {
         return;
@@ -62,16 +65,17 @@ function setPlant() {
     currPlantTile.appendChild(plant);
 }
 
+
 function selectTile() {
     if (gameOver) {
         return;
     }
     if (this == currMoleTile) {
         score += 10;
-        document.getElementById("score").innerText = score.toString(); //update score html
+        document.getElementById("score").innerText = score.toString();   //update score html
     }
     else if (this == currPlantTile) {
-        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); //update score html
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();   //update score html
         gameOver = true;
     }
 }
